2f986d43f56b967023e659a346f55e94f120b656
Caelin Hargrave <106052558+Caelinh@users.noreply.github.com>
2022-08-04 13:48:19 -0500

    Revert "Revert "Auth""

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import Main from './components/Main.js';
 import React from 'react';
-
+import Login from './components/auth/Login.js';
+import Auth0Token from './Auth0Token.js';
 
 class App extends React.Component {
   render() {
     return (
-      <Main />
-    )
+      <div>
+        <Main />
+        <Login />
+        <Auth0Token />
+      </div>
+      
+    );
     }
   
   
