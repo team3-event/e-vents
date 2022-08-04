65d562c3fef2aaeffe9182ececdc08a462fb9326
Caelin Hargrave <106052558+Caelinh@users.noreply.github.com>
2022-08-04 13:03:53 -0500

    Revert "auth working"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 
 import Main from './components/Main.js';
 import React from 'react';
-import Login from './components/auth/Login.js';
-import Auth0Token from './Auth0Token.js';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Main />
-        <Login />
-        <Auth0Token />
-      </div>
-      
-    );
+      <Main />
+    )
     }
   
   
