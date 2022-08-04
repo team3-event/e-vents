dc6856a705055a1d1a6296f34f9a73223e928273
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-08-04 14:17:40 -0500

    fixing main branch

---
+++ src/Auth0Token.js
@@ -0,0 +1,43 @@
+import React,{ Component } from 'react';
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
+
+class Auth0Token extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            list: []
+        }
+    }
+
+    async componentDidMount() {
+        console.log(this.props);
+        if (this.props.auth0.isAuthenticated) {
+            try {
+                let res = await this.props.auth0.getIdTokenClaims();
+
+                let token = res.__raw;
+                console.log(token);
+                let response = await axios.get('http://localhost:3001/login', { headers: { "Authorization": `Bearer ${token}` } });
+                console.log(response);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.list.map(value => (
+                    <p>{value.name}</p>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default withAuth0(Auth0Token);
