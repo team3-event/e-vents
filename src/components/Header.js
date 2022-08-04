f2c7b54cd571453a954c047bb38ec85909630192
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-08-04 13:47:22 -0500

    last try to fix this

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>E-VENT</h1>
+                <h1>E-VENT hopefully this fixes it</h1>
                 
             </div>
         )
