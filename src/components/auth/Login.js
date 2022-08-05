102dfc0d8e42e5d40d226690ae21fe3d4614ea15
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-08-05 01:18:13 -0500

    updated style

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
 
+
 function Login() {
 
     const { isAuthenticated, loginWithRedirect, error, logout } = useAuth0();
     return !isAuthenticated 
-    ? <button onClick={loginWithRedirect}>Login</button>
+    ? <button onClick={loginWithRedirect} type="button" className="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-cyan-900 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >Login</button>
     : <button onClick={logout}>Logout</button>
 }
 
