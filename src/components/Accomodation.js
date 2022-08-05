8a5bdcac2aeaef711db1c3970fcc47ed6a700e92
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-08-04 21:50:15 -0500

    added buttons to select events and store data on server

--- src/components/Accomodation.js
+++ src/components/Accomodation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MailIcon, PhoneIcon, } from '@heroicons/react/solid'
+import { MailIcon, PhoneIcon, GlobeIcon } from '@heroicons/react/solid'
 
 
 
@@ -11,7 +11,7 @@ class Accomodation extends React.Component {
 
             <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {this.props.queryData.map((hotel) => (
-                    <AccItem name={hotel.name} price={hotel.bestPrice} />))}
+                    <AccItem getHotel={this.props.getHotel} hotelInfo={hotel} name={hotel.name} price={hotel.bestPrice} />))}
             </ul>
 
 
@@ -21,41 +21,35 @@ class Accomodation extends React.Component {
 
 
 class AccItem extends React.Component {
+
+    saveHotel = () => {
+        this.props.getHotel(this.props.hotelInfo)
+    }
+    
     render() {
         return (
             <li key={this.props.name} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
                 <div className="w-full flex items-center justify-between p-6 space-x-6">
                     <div className="flex-1 truncate">
                         <div className="flex items-center space-x-3">
-                            <h3 className="text-gray-900 text-sm font-medium truncate">{this.props.name}</h3>
-                            <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                                {this.props.name}
-                            </span>
+                            <h3 className="text-gray-900 text-lg text-align text-center font-medium">{this.props.name}</h3>
+
                         </div>
-                        <p className="mt-1 text-gray-500 text-sm truncate">{this.props.name}</p>
+
                     </div>
-                    <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={this.props.name} alt="" />
+
                 </div>
                 <div>
                     <div className="-mt-px flex divide-x divide-gray-200">
                         <div className="w-0 flex-1 flex">
-                            <a
-                                href={`mailto:${this.props.name}`}
-                                className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                            <button onClick={this.saveHotel}
+                                type="button"
+                                className="block items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
-                                <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                                <span className="ml-3">Email</span>
-                            </a>
+                                Select
+                            </button>
                         </div>
-                        <div className="-ml-px w-0 flex-1 flex">
-                            <a
-                                href={`tel:${this.props.name}`}
-                                className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
-                            >
-                                <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                                <span className="ml-3">Call</span>
-                            </a>
-                        </div>
+
                     </div>
                 </div>
             </li>
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -4,29 +4,69 @@ import Card from "react-bootstrap/Card"
 
 
 class Travel extends React.Component {
-    
     render() {
         return (
-            <div>
-                <Row>
-                <Card  bg="primary" text="light" style={{height:"20rem", width: '18rem' }}>
-                    <Card.Body>
-                    <Card.Title>{this.props.name}</Card.Title>
-                    <Card.Text>
-                    Price: {this.props.price}
-                    </Card.Text>
-                    <Card.Text>dtime: {this.props.dTime}</Card.Text>
-                    <Card.Text>aTime: {this.props.aTime}</Card.Text>
-                    <Card.Text>stop: {this.props.stop}</Card.Text>
-             
-                 </Card.Body>
-                    
-                </Card>
-                </Row>
+            <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <li key={this.props.price} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+                    <div className="w-full flex items-center justify-between p-6 space-x-6">
+                        <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                                <h3 className="text-gray-900 text-lg text-align text-center font-medium">{this.props.price}</h3>
 
+                            </div>
 
+                        </div>
 
-            </div>
+                    </div>
+                    <div className="w-full flex items-center justify-between p-6 space-x-6">
+                        <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                                <h3 className="text-gray-900 text-lg text-align text-center font-medium">{this.props.url}</h3>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="w-full flex items-center justify-between p-6 space-x-6">
+                        <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                                <h3 className="text-gray-900 text-lg text-align text-center font-medium">{this.props.dtime}</h3>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="w-full flex items-center justify-between p-6 space-x-6">
+                        <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                                <h3 className="text-gray-900 text-lg text-align text-center font-medium">{this.props.atime}</h3>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <div className="-mt-px flex divide-x divide-gray-200">
+                            <div className="w-0 flex-1 flex">
+                                <button
+                                    type="button"
+                                    className="block items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Select
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                    <button  type="button" onClick={this.props.journey}
+                                    className="block items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            Save Journey
+                    </button>
+                </li>
+            </ul>
         )
     }
 }
