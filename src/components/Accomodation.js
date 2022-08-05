4e00fd04f69afcd503ded4b3b8e62e601af76c9d
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-08-04 19:53:57 -0500

    added env pathing

--- src/components/Accomodation.js
+++ src/components/Accomodation.js
@@ -1,46 +1,69 @@
 import React from "react";
-import Card from "react-bootstrap/Card"
-import Row from "react-bootstrap/Row"
+import { MailIcon, PhoneIcon, } from '@heroicons/react/solid'
 
 
 
 
 class Accomodation extends React.Component {
-    
+
     render() {
         return (
-            <div>
-                <Row>
-                {this.props.queryData.map(hotel =>
-                
-                <Card key={hotel.name} bg="primary" text="light" style={{height:"20rem", width: '18rem' }}>
-                    <AccomodationCards  name={hotel.name} price={hotel.bestPrice}/>
-                </Card>)}
-                </Row>
+
+            <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {this.props.queryData.map((hotel) => (
+                    <AccItem name={hotel.name} price={hotel.bestPrice} />))}
+            </ul>
 
 
-
-            </div>
         )
     }
 }
 
 
-
-class AccomodationCards extends React.Component {
-
+class AccItem extends React.Component {
     render() {
         return (
-            <div>
-                <Card.Body>
-                    <Card.Title>{this.props.name}</Card.Title>
-                    <Card.Text>
-                    Price: {this.props.price.price1}
-                    </Card.Text>
-                </Card.Body>
-            </div>
+            <li key={this.props.name} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+                <div className="w-full flex items-center justify-between p-6 space-x-6">
+                    <div className="flex-1 truncate">
+                        <div className="flex items-center space-x-3">
+                            <h3 className="text-gray-900 text-sm font-medium truncate">{this.props.name}</h3>
+                            <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                                {this.props.name}
+                            </span>
+                        </div>
+                        <p className="mt-1 text-gray-500 text-sm truncate">{this.props.name}</p>
+                    </div>
+                    <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={this.props.name} alt="" />
+                </div>
+                <div>
+                    <div className="-mt-px flex divide-x divide-gray-200">
+                        <div className="w-0 flex-1 flex">
+                            <a
+                                href={`mailto:${this.props.name}`}
+                                className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                            >
+                                <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                                <span className="ml-3">Email</span>
+                            </a>
+                        </div>
+                        <div className="-ml-px w-0 flex-1 flex">
+                            <a
+                                href={`tel:${this.props.name}`}
+                                className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                            >
+                                <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                                <span className="ml-3">Call</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+
         )
     }
 }
+
 
 export default Accomodation;
